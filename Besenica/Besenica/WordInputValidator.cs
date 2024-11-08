bb99ec440e77627d9148ace2cf91591760199b76
JivkoSp <atjspasov@gmail.com>
2024-11-08 18:10:51 +0200

    Added new class: WordInputValidator.cs

---
+++ Besenica/Besenica/WordInputValidator.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Besenica
+{
+    internal sealed class WordInputValidator
+    {
+        public static string ValidateWordInput()
+        {
+            string word = "";
+
+            do
+            {
+                Console.Write("\nEnter a word: ");
+
+                word = WordInput.GetWordFromUser();
+
+                if (string.IsNullOrEmpty(word))
+                {
+                    Console.WriteLine(" *** The word is not valid! ***");
+                    Console.WriteLine(" Please enter a valid word.");
+                }
+            }
+            while (string.IsNullOrEmpty(word));
+
+            return word;
+        }
+    }
+}
