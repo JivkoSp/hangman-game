d15701397899bb456125ed69b95a0b6ccb2b2c13
JivkoSp <atjspasov@gmail.com>
2024-11-08 16:23:57 +0200

    Project init. Created a Options class that contains DisplayMainMenu and DisplayGameMenu methods, that are responsible for displaying different options for the user.

---
+++ Besenica/Besenica/Options.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Besenica
+{
+    internal sealed class Options
+    {
+        public static void DisplayMainMenu()
+        {
+            Console.WriteLine("\n *** Main Menu *** \n");
+            Console.WriteLine("1. Start a game");
+            Console.WriteLine("2. View your score");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine();
+        }
+
+        public static void DisplayGameMenu()
+        {
+            Console.WriteLine("\n *** Game Menu *** \n");
+            Console.WriteLine("1. View the game rules");
+            Console.WriteLine("2. Enter a word");
+            Console.WriteLine("3. Guess a random word");
+            Console.WriteLine("4. Go back");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Besenica/Besenica/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Besenica
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            bool exitGame = false;
+
+            while (!exitGame)
+            {
+                Options.DisplayMainMenu();
+
+                Console.Write("\n >> ");
+
+                string? userChoice = Console.ReadLine()?.Trim();
+
+                switch (userChoice)
+                {
+                    case "1":
+                        Console.Clear();
+                        Options.DisplayGameMenu();
+                        // TODO: Must handle the interactions with the game menu
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Feature not yet implemented.");
+                        break;
+                    case "3":
+                        exitGame = true;
+                        Console.WriteLine("\nExiting the game. Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
