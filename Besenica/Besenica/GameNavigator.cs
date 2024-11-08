0831f7de25a11fe5fb5ebac00a89a8051fb167a0
JivkoSp <atjspasov@gmail.com>
2024-11-08 16:46:09 +0200

    Added new class: GameNavigator.cs

---
+++ Besenica/Besenica/GameNavigator.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Besenica
+{
+    internal sealed class GameNavigator
+    {
+        public static void HandleGameMenu()
+        {
+            Console.Write("\n>> ");
+
+            string? gameChoice = Console.ReadLine()?.Trim();
+
+            switch (gameChoice)
+            {
+                case "1":
+                    Options.DisplayGameRules();
+                    break;
+                case "2":
+                    // TODO
+                    // validate the word input
+                    // create separate class for handling the core game logic
+                    break;
+                case "3":
+                    // TODO
+                    // open a txt file and get a random word from it.
+                    break;
+                case "4":
+                    return; // Go back to the main menu
+                default:
+                    Console.WriteLine("\nInvalid option. Please try again.");
+                    break;
+            }
+        }
+    }
+}
