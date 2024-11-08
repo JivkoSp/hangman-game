f4c7664e61dd7e2a5b735c47e62248f6686ad9d3
JivkoSp <atjspasov@gmail.com>
2024-11-08 20:23:44 +0200

    Added new class - FileManager.cs

---
+++ Besenica/Besenica/FileManager.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Besenica
+{
+    internal static class FileManager
+    {
+        public static void CreateAndInitializeFileWithWords(string filePath = "words.txt")
+        {
+            var words = WordGenerator.GenerateRandomWords();
+
+            File.WriteAllLines(filePath, words);
+        }
+
+        public static string GetRandomWordFromFile(string filePath)
+        {
+            string[] words = File.ReadAllLines(filePath);
+
+            if (words.Length == 0)
+            {
+                return "No words found in the file.";
+            }
+
+            Random random = new Random();
+
+            int randomIndex = random.Next(words.Length);
+
+            return words[randomIndex];
+        }
+    }
+}
